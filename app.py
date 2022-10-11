45e7957d4bc826ef5e7b0989e87660f465e5aece
joaobarbosasoitic <joao.barbosa@soitic.com>
2022-10-11 16:29:43 -0300

    verifica se as listas antigas estão vazias

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import os 
-from win10toast import ToastNotifier
+import webbrowser
+from win10toast_click import ToastNotifier
 
 
 if os.path.exists('./data') == False:
@@ -11,7 +12,6 @@ if os.path.exists('./data') == False:
     leiautes = []
     notasTecnicas = []
 
-
     toaster = ToastNotifier()
     r = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica/documentacao-tecnica')
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -43,82 +43,53 @@ if os.path.exists('./data') == False:
     with open('./data/NotasTecnicas.txt', 'r', encoding="utf-8") as texto2:
         notasTecnicas_old = texto2.readlines()
 
-
-    if len(leiautes_old) == len(leiautes) and len(notasTecnicas_old) == len(notasTecnicas):
-        toaster.show_toast("Não houve alterações no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
-    else:
-        toaster.show_toast("Houve uma alteração no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
-
-
-elif os.path.exists('./data/leiaute.txt') == False and os.path.exists('./data/leiaute.txt') == False:
-    file_leiaute = open('./data/leiaute.txt', 'w+', encoding="utf-8")
-    file_NotasTecnicas = open('./data/NotasTecnicas.txt', 'w+', encoding="utf-8")
-    leiautes = []
-    notasTecnicas = []
-
-
-    toaster = ToastNotifier()
-    r = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica/documentacao-tecnica')
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-
-    leiautes_request = soup.find_all('a',{'class':'outstanding-link'})
-    notasTecnicas_request = soup.find_all('a',{'class':'internal-link'})
-
-
-    for nota in notasTecnicas_request:
-        notasTecnicas.append(nota.text)
-
-    for documento in leiautes_request:
-        leiautes.append(documento.text)
-
-
-    for i in leiautes:
-        file_leiaute.writelines(f'{i}\n')
-
-    for j in notasTecnicas:
-        file_NotasTecnicas.writelines(f'{j}\n')
-
-    file_leiaute.close()
-    file_NotasTecnicas.close()
-
-    with open('./data/leiaute.txt', 'r', encoding="utf-8") as texto:
-        leiautes_old = texto.readlines()
-
-    with open('./data/NotasTecnicas.txt', 'r', encoding="utf-8") as texto2:
-        notasTecnicas_old = texto2.readlines()
-
-
-    if len(leiautes_old) == len(leiautes) and len(notasTecnicas_old) == len(notasTecnicas):
-        toaster.show_toast("Não houve alterações no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
-    else:
-        toaster.show_toast("Houve uma alteração no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
 else:
+    browser = 'https://www.gov.br/esocial/pt-br/documentacao-tecnica/documentacao-tecnica'
     leiautes = []
     notasTecnicas = []
 
     toaster = ToastNotifier()
     r = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica/documentacao-tecnica')
     soup = BeautifulSoup(r.text, 'html.parser')
-
-
     leiautes_request = soup.find_all('a',{'class':'outstanding-link'})
     notasTecnicas_request = soup.find_all('a',{'class':'internal-link'})
-
 
     for nota in notasTecnicas_request:
         notasTecnicas.append(nota.text)
 
-    for documento in leiautes_request:
-        leiautes.append(documento.text)
+    for leiaute in leiautes_request:
+        leiautes.append(leiaute.text)
 
     with open('./data/leiaute.txt', 'r', encoding="utf-8") as texto:
         leiautes_old = texto.readlines()
 
     with open('./data/NotasTecnicas.txt', 'r', encoding="utf-8") as texto2:
         notasTecnicas_old = texto2.readlines()
-   
-    if len(leiautes_old) == len(leiautes) and len(notasTecnicas_old) == len(notasTecnicas):
+
+    if len(notasTecnicas_old) == 0:
+            file_NotasTecnicas = open('./data/NotasTecnicas.txt', 'w+', encoding="utf-8")
+            for i in notasTecnicas:
+                notasTecnicas_old.append(i)
+                file_NotasTecnicas.writelines(f'{i}\n')
+                     
+    if len(leiautes_old) == 0:
+            file_leiaute = open('./data/leiaute.txt', 'w+', encoding="utf-8")
+            for j in leiautes:
+                leiautes_old.append(j)
+                file_leiaute.writelines(f'{j}\n')
+                 
+
+    if len(leiautes_old) == len(leiautes) or len(notasTecnicas_old) == len(notasTecnicas):
+        file_leiaute = open('./data/leiaute.txt', 'w+', encoding="utf-8")
+        for j in leiautes:
+            leiautes_old.append(j)
+            file_leiaute.writelines(f'{j}\n')
+        
+        file_NotasTecnicas = open('./data/NotasTecnicas.txt', 'w+', encoding="utf-8")
+        for i in notasTecnicas:
+            notasTecnicas_old.append(i)
+            file_NotasTecnicas.writelines(f'{i}\n')
+        
         toaster.show_toast("Não houve alterações no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
     else:
         toaster.show_toast("Houve uma alteração no eSocial", "teste notificação", icon_path='./assets/esocial.ico', duration=10)
--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-class 
+from bs4 import BeautifulSoup
+import requests
+import os 
+from win10toast import ToastNotifier
+
+class Alerta():
+    def scraping(self):
+        self.toaster = ToastNotifier()
+        r = requests.get('https://www.gov.br/esocial/pt-br/documentacao-tecnica/documentacao-tecnica')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        leiautes_request = soup.find_all('a',{'class':'outstanding-link'})
+        notasTecnicas_request = soup.find_all('a',{'class':'internal-link'})
+
+    def populaListas(self):
+        leiautes = []
+        notasTecnicas = []
+        for nota in self.notasTecnicas_request:
+            notasTecnicas.append(nota.text)
+
+        for documento in self.leiautes_request:
+            leiautes.append(documento.text)
+
+    def salvaLista(self, file_NotasTecnicas, file_leiaute):
+        for i in self.leiautes:
+            file_leiaute.writelines(f'{i}\n')
+
+        for j in self.notasTecnicas:
+            file_NotasTecnicas.writelines(f'{j}\n')
+        
+        self.file_leiaute.close()
+        self.file_NotasTecnicas.close()
+    
+    def populaListaAntiga(self):
+        with open('./data/leiaute.txt', 'r', encoding="utf-8") as texto:
+            self.leiautes_old = texto.readlines()
+
+        with open('./data/NotasTecnicas.txt', 'r', encoding="utf-8") as texto2:
+            self.notasTecnicas_old = texto2.readlines()
